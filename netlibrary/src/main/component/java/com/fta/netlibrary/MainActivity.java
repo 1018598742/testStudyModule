74b5d6deb966e116895ece98954b15a91054f7f4
fta <1018598742@qq.com>
2019-08-13 19:17:19 +0800

    study module

---
+++ netlibrary/src/main/component/java/com/fta/netlibrary/MainActivity.java
@@ -0,0 +1,17 @@
+package com.fta.netlibrary;
+
+import android.os.Bundle;
+
+import com.fta.teststudymodule.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card2_activity_main);
+    }
+}
---
+++ netlibrary/src/main/java/com/fta/netlibrary/SecondActiviy.java
@@ -0,0 +1,7 @@
+package com.fta.netlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActiviy extends AppCompatActivity {
+
+}
